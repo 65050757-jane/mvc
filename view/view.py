2571eb1ea0dbbdd05b30803a43198cd21a3148ed
unknown <65050757@kmitl.ac.th>
2024-09-15 09:00:05 +0700

    start create file with some code

--- view/view.py
+++ view/view.py
@@ -4,7 +4,7 @@ class MainView:
     def __init__(self, root, controller):
         self.root = root
         self.controller = controller
-        self.root.title("")
+        self.root.title("Cow Management System")
         self.root.geometry("600x400")
 
     def create_window(self):
